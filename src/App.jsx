81be88a4884937dce638779f4b6cb0614fd384f8
Ivan42210 <ivan.cha.42@outlook.fr>
2024-07-26 18:07:45 +0200

    mise en p^lace

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import {BrowserRouter as Router} from 'react-router-dom'
+
+import './App.css'
+import AppRouter from './AppRouter'
+
+function App() {
+
+
+  return (
+    <>
+      <Router>
+          <AppRouter/>
+      </Router>
+    </>
+  )
+}
+
+export default App
