90d9b7e01b9fa5085839ec08c2ab176cddedc639
Ivan42210 <ivan.cha.42@outlook.fr>
2024-07-27 13:08:45 +0200

    modification utils

---
+++ src/Utils/API.js
@@ -0,0 +1,32 @@
+import axios from 'axios'
+
+
+const getProducts = async () => {
+    const res = await axios.get("https://fakestoreapi.com/products");
+    const datas = res.data;
+    return datas
+}
+
+
+const getAllUsers = async () => {
+    const res = await axios.get('https://fakestoreapi.com/users');
+    const datas = res.data;
+    return datas
+}
+
+const getAllCarts = async () => {
+    const res = await axios.get('https://fakestoreapi.com/carts');
+    const datas = res.data;
+    return datas
+}
+
+const getUserCart = async (userId) => {
+    const res = await axios.get(`https://fakestoreapi.com/carts/user/${userId}`);
+    const datas = res.data;
+    return datas
+}
+
+
+
+
+export { getProducts, getAllUsers, getAllCarts, getUserCart }
